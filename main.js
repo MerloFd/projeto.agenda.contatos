717cecb8b4c7f31b16e65f16725583abf9b276b4
Matheus Merlo <matheus.merlo02@gmail.com>
2023-09-28 11:58:15 -0300

    fix excluir linha

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 const form = document.getElementById('form-contatos');
-const clickExcluir = document.getElementsByClassName('excluir');
+const corpoTable = document.querySelector('tbody');
 let linhas = ''; //para criar novas linhas. Global pois vai ser usada em outras functions
 
 const ligarImg = '<img class="telefonar-img" src="./images/telefone-img.png" alt="telefonar">'
 const mensagemImg = '<img class="whatsapp-img" src="./images/whatsapp-logo.png" alt="enviar mensagem"></td>'
 
-const buttonExcluir = '<button class="excluir" type="reset"><img class="lixeira-icon" src="./images/lixeira-icon.png" alt="excluir contatos"></button>'
+const buttonExcluir = '<button class="excluir" type="button"><img class="lixeira-icon" src="./images/lixeira-icon.png" alt="excluir contatos"></button>'
+
+document.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('Botão Clicado2');
+
+    //atribui o alvo do evento para a const target
+    const target = e.target;
+
+    //se alvo tiver como classe 'excluir', execute X
+    if (target.classList.contains('excluir')) {
+        //chama a function com o alvo de argumento
+        excluirLinha(target);
+    }
+})
 
 form.addEventListener('submit', function(e){ //"gatilho" com o button que inicia os eventos
     e.preventDefault();
 
+    console.log('Botão Clicado1');
+
     adicionaLinha();
-    atualizaTable()
+    atualizaTable();
 })
 
-clickExcluir.addEventListener('click', function(e){
-    e.preventDefault();
-
-    form.innerHTML = ligarImg;
-
-})
-
-function adicionaLinha (){
-
+function adicionaLinha(){
     const inputContato = document.getElementById('contato');
     const inputTel = document.getElementById('telefone');
 
@@ -44,6 +52,15 @@ function adicionaLinha (){
 
 function atualizaTable(){
     //da um innerHTML para escrever o conteúdo do adicionaLinha no HTML
-    const corpoTable = document.querySelector('tbody');
     corpoTable.innerHTML = linhas;
+}
+
+//recebe como parâmetro o button
+function excluirLinha(button){
+        //usa o closest para encontrar o tr mais próximo do button
+        const conteudoLinha = button.closest('tr');
+
+        //remove a linha do conteúdo e atualizar a tabela
+        linhas = linhas.replace(conteudoLinha.outerHTML, '');
+        atualizaTable();
 }
